4d8830f3e915086a14772dfa84da468843a7272e
Ben Thomas <benjamin.h.thomas1@gmail.com>
2025-05-26 11:39:29 +0200

    edited fetch url to match nginx proxy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,20 @@
 // import { useState } from 'react'
 import './App.css'
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [message, setMessage] = useState<string | null>(null);
+  useEffect(() => {
+	fetch('/api/', {
+		credentials: 'include'
+	})
+	.then(response => response.text())
+	.then(setMessage);
+  }, []);
   return (
 	<div className="flex flex-col h-screen">
 		<div className="flex flex-grow justify-center items-center">
-			Hello There
+			{message ?? "Loading..."}
 		</div>
 	</div>
   )
