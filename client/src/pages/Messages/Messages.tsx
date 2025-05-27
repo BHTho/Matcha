ba852724ac835bf7078f3887b3e1f090a3cd0b7e
bthomas <bthomas@Inception.myguest.virtualbox.org>
2025-05-27 11:49:12 +0200

    added pages for explore, messages, profile, and settings

---
+++ client/src/pages/Messages/Messages.tsx
@@ -0,0 +1,12 @@
+import styles from './MessagePage.module.css';
+
+
+function MessagePage() {
+    return (
+        <div className={styles.MessagePage}>
+            Message Page
+        </div>
+    )
+}
+
+export default MessagePage;
