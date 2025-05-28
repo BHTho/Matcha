70df1cf9896ece54324c812bb54d4c509ff25fff
Ben Thomas <benjamin.h.thomas1@gmail.com>
2025-05-28 18:53:20 +0200

    populated explore page with placeholder card and swipe buttons

--- client/src/pages/Explore/Explore.tsx
+++ client/src/pages/Explore/Explore.tsx
@@ -1,9 +1,14 @@
 import styles from './Explore.module.css';
+import ProfileCard from '../../components/profileCard/ProfileCard';
 
 function Explore() {
     return (
         <div className={styles.ExplorePage}>
-            Explore page
+            <ProfileCard imgPath='src/assets/bazza.png' />
+            <div className={styles.decisionBar}>
+                <button><img src="src/assets/heart.svg" alt="loveIcon" /></button>
+                <button><img src="src/assets/cross.svg" alt="crossIcon" /></button>
+            </div>
         </div>
     )
 }
