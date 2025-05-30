cb324344d9c8fb7c759a624300430386d12326b9
Ben Thomas <benjamin.h.thomas1@gmail.com>
2025-05-30 15:37:51 +0200

    made chat more modern. Header uneeded. Desktop and mobile will have entirely different css

--- client/src/pages/Messages/Messages.tsx
+++ client/src/pages/Messages/Messages.tsx
@@ -1,10 +1,23 @@
 import styles from './Messages.module.css';
+import backIcon from '../../assets/back-button.svg';
 
+/*
+To do:
+    leave format unchanged on large desktop screen.
+    On small/mobile screen have it so that thumbnail takes up 100% of screen
+    When tumbnail clicked, full chat takes up 100% of screen.
+    back button (only real on small screen) makes thumbail take up full screen
+*/
 
 function MessagePage() {
     return (
         <div className={styles.MessagePage}>
-            Message Page
+            <img className={styles.BackButton} src={backIcon} alt="backBtn" />
+            <div className={styles.MessageThumbnails}>
+                <div className={styles.Thumbnail}>Example Thumbnail</div>
+            </div>
+            <div className={styles.ChatWindow}>full chats here</div>
+            <div className={styles.ChatInput}>Chat Input here</div>
         </div>
     )
 }
