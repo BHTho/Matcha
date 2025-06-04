8c9744afc6fcfcca035c5568f33c49d031edd5b9
Ben Thomas <benjamin.h.thomas1@gmail.com>
2025-06-04 11:53:12 +0200

    added names to dummy data

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -4,6 +4,8 @@ import PageParentDiv from '../../components/PageParentDiv/PageParentDiv';
 interface UserProfile {
     id: string,
     username: string,
+    firstName: string,
+    lastName: string,
     email: string,
     bio: string,
     tags: string[],
@@ -11,11 +13,14 @@ interface UserProfile {
     gender: string, // male, female, other
     accntCoords: string[], // GPS coordinates (not live)
     city: string,
+    status: string, // online, offline
 }
 
 const dummyUser : UserProfile = {
     id: "1",
-    username: "dingus",
+    username: "ElonLover123",
+    firstName: "Dingus",
+    lastName: "McGee",
     email: "dingus@42.fr",
     bio: "Howdy there I'm Dingus. I love C++ and I can't talk to women.",
     tags: ["#linux","#dev","#IloveLinusTorvalds","#IuseArchBTW"],
@@ -23,6 +28,7 @@ const dummyUser : UserProfile = {
     gender: "male",
     accntCoords: ["48.8575", "2.3514"], // Paris Coords
     city: "Paris",
+    status: "online",
 }
 
 function Profile() {
@@ -31,8 +37,10 @@ function Profile() {
             <div className={styles.ProfileParent}>
                 <div className={styles.ProfileHeader}>
                     <img className={styles.ProfileAvatar} src="default_avatar.svg" alt="default_avatar" />
-                    <h1 className={styles.ProfileName}>Dingus McGee</h1>
-                    <h1 className={styles.ProfileCity}>{dummyUser.city}</h1>
+                    <div className={styles.ProfileNameParent}>
+                        <h1 className={styles.ProfileName}>{dummyUser.firstName} | {dummyUser.city}</h1>
+                        <div className={styles[dummyUser.status]}></div>
+                    </div>
                 </div>
                 <div className={styles.ProfileBio}>
                     {dummyUser.bio}
