6edb2c774103bdb08ca5be545e10520c42f46b47
Ben Thomas <benjamin.h.thomas1@gmail.com>
2025-06-07 13:34:01 +0200

    imported temp images using glob import

--- client/src/components/ProfileImgStack/ProfileImgStack.tsx
+++ client/src/components/ProfileImgStack/ProfileImgStack.tsx
@@ -8,9 +8,15 @@ interface ProfileImgStackProps {
 };
 
 const ProfileImgStack: React.FC<ProfileImgStackProps> = ({ userData }) => {
+    // replace this with an API call
+    const images = import.meta.glob('/src/data/dummy_photos/user_1/*.{png,jpg,jpeg}', {eager: true});
+    const imageList = Object.values(images).map((image, index) => (
+        <img key={index} src={image.default} alt="profile_image" className={styles.StackImg}/>
+    ));
+
     return (
         <div className={styles.ImageStackParent}>
-            <img className={styles.StackImg} src={`/api/images/user_${userData.id}/${userData.id}_0.jpg`} alt="" />
+            {imageList}
         </div>
     )
 }
