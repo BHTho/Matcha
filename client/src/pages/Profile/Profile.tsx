e00c89a6125bd88cd06431152320cfc7cd224e4a
Ben Thomas <benjamin.h.thomas1@gmail.com>
2025-06-07 10:46:11 +0200

    simplified profile page

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,55 +1,17 @@
 import styles from './Profile.module.css';
 import PageParentDiv from '../../components/PageParentDiv/PageParentDiv';
+import dummyUser from '../../data/profileData/';
+import ProfileImgStack from '../../components/ProfileImgStack/ProfileImgStack';
 
-interface UserProfile {
-    id: string,
-    username: string,
-    firstName: string,
-    lastName: string,
-    email: string,
-    bio: string,
-    tags: string[],
-    sexuality: string, // straight, gay, bi
-    gender: string, // male, female, other
-    accntCoords: string[], // GPS coordinates (not live)
-    city: string,
-    status: string, // online, offline
-}
-
-const dummyUser : UserProfile = {
-    id: "1",
-    username: "ElonLover123",
-    firstName: "Dingus",
-    lastName: "McGee",
-    email: "dingus@42.fr",
-    bio: "Howdy there I'm Dingus. I love C++ and I can't talk to women.",
-    tags: ["#linux","#dev","#IloveLinusTorvalds","#IuseArchBTW"],
-    sexuality: "Straight",
-    gender: "Male",
-    accntCoords: ["48.8575", "2.3514"], // Paris Coords
-    city: "Paris",
-    status: "online",
-}
 
 function Profile() {
     return (
         <PageParentDiv>
             <div className={styles.ProfileParent}>
-                <div className={styles.ProfileHeader}>
-                    <img className={styles.ProfileAvatar} src="default_avatar.svg" alt="default_avatar" />
-                    <div className={styles.ProfileNameParent}>
-                        <h1 className={styles.ProfileName}>{dummyUser.firstName} | {dummyUser.city}</h1>
-                        <div className={styles[dummyUser.status]}></div>
-                    </div>
-                <div className={styles.ProfileAttr}>
-                    Gender: {dummyUser.gender} | Sexuality: {dummyUser.sexuality}
-                </div>
-                </div>
-                <div className={styles.ProfileBio}>
-                    {dummyUser.bio}
-                </div>
+                <ProfileImgStack userData={dummyUser}/>
+                <div className={styles[dummyUser.status]}></div>
                 <div className={styles.ProfileTags}>
-                    {dummyUser.tags.map((tag) => {
+                    {dummyUser.tags.map((tag: string) => {
                         return tag + " ";
                     })}
                 </div>
